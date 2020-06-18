dcc4723bb1fb37766914481572220c448d591497
parikshitg <parikshit@urantiatech.com>
2020-06-18 12:36:24 +0530

    create and read user

--- handler.go
+++ handler.go
@@ -4,34 +4,102 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
 
+type User struct {
+	Id        string
+	Username  string
+	Password  string
+	CreatedAt time.Time
+}
+
+func CreateUser(username, password string) {
+
+	createdAt := time.Now()
+
+	_, err := db.Exec(`INSERT INTO userss (username, password, created_at) VALUES (?, ?, ?)`, username, password, createdAt)
+	if err != nil {
+		log.Fatal("Insert Error : ", err)
+	}
+}
+
+func ReadUser(username, password string) (string, string) {
+
+	query := "SELECT username, password FROM userss WHERE username = ?"
+	if err := db.QueryRow(query, username).Scan(&username, &password); err != nil {
+		log.Fatal("Read User Error : ", err)
+	}
+
+	return username, password
+}
+
+// Login Controller
 func Login(w http.ResponseWriter, r *http.Request) {
 
-	page, err := template.ParseFiles("templates/login.html")
-	if err != nil {
-		log.Fatal("ParseFiles: ", err)
+	if r.Method == http.MethodPost {
+
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+
+		dbusername, dbpassword := ReadUser(username, password)
+
+		if username == dbusername && password == dbpassword {
+			pages := template.Must(template.ParseFiles("templates/dashboard.html"))
+
+			err := pages.Execute(w, nil)
+			if err != nil {
+				log.Fatal("Execute : ", err)
+			}
+		}
+
+	} else {
+
+		page, err := template.ParseFiles("templates/login.html")
+		if err != nil {
+			log.Fatal("ParseFiles: ", err)
+		}
+
+		err = page.Execute(w, nil)
+		if err != nil {
+			log.Fatal("Execute:", err)
+		}
 	}
 
-	err = page.Execute(w, nil)
-	if err != nil {
-		log.Fatal("Execute:", err)
-	}
 }
 
+// Register Controller
 func Register(w http.ResponseWriter, r *http.Request) {
 
-	page, err := template.ParseFiles("templates/register.html")
-	if err != nil {
-		log.Fatal("ParseFiles :", err)
-	}
+	if r.Method == http.MethodPost {
 
-	err = page.Execute(w, nil)
-	if err != nil {
-		log.Fatal("Execute:", err)
+		username := r.FormValue("username")
+		password := r.FormValue("password")
+		password2 := r.FormValue("password2")
+
+		if password != password2 {
+			log.Fatal("password not equal to password 2")
+			return
+		}
+
+		CreateUser(username, password)
+
+	} else {
+
+		page, err := template.ParseFiles("templates/register.html")
+		if err != nil {
+			log.Fatal("ParseFiles :", err)
+		}
+
+		err = page.Execute(w, nil)
+		if err != nil {
+			log.Fatal("Execute:", err)
+		}
+
 	}
 }
 
+// Dashboard Controller
 func Dashboard(w http.ResponseWriter, r *http.Request) {
 
 	page, err := template.ParseFiles("templates/dashboard.html")
--- main.go
+++ main.go
@@ -1,10 +1,44 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
+var db *sql.DB
+
 func main() {
+
+	var err error
+	// Create a database
+	db, err = sql.Open("mysql", "root:parikshitg@tcp(127.0.0.1:3306)/testingdb")
+	if err != nil {
+		fmt.Println("Db Open Error:", err)
+	}
+
+	defer db.Close()
+
+	fmt.Println("Successfully connected to Database.")
+
+	// Create a database table
+	query := `
+            CREATE TABLE userss (
+                id INT AUTO_INCREMENT,
+                username TEXT NOT NULL,
+                password TEXT NOT NULL,
+                created_at DATETIME,
+                PRIMARY KEY (id)
+            );`
+
+	if _, err := db.Exec(query); err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Successfully Create Table")
 
 	http.Handle("/login", http.HandlerFunc(Login))
 	http.Handle("/register", http.HandlerFunc(Register))
